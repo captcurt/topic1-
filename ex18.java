dc2f4d752787d12f710d801301cd77c186f5b61b
captcurt <JCurtner1@my.gcu.edu>
2017-02-21 23:00:57 -0600

    Add files via upload
    excersie 1.8

---
+++ ex18.java
@@ -0,0 +1,16 @@
+
+public class ex18 {
+
+	public static void main(String[] args) {
+		System.out.println("Find the area of a circle");
+		
+		int radius = 0;
+		if (radius >= 0) {
+			int PI = 0;
+			int area = radius * radius * PI;
+			System.out.println("The area for the circle" + radius + " is " + area);
+		}
+
+	}
+
+}
