50765e87decb9add085be961f1c1634cb54df5e7
captcurt <JCurtner1@my.gcu.edu>
2017-02-22 23:36:42 -0600

    Add files via upload
    average speed

---
+++ averagespeed.java
@@ -0,0 +1,13 @@
+public class averagespeed {
+	public static void main(String[] args) {
+	  System.out.println("Find the average speed in miles.");
+	  System.out.println("Distance = 14km \nTime = 45 minutes 30 seconds = (45 x 60) + 30 \n"
+			    + "                               -------------- hours \n"
+			    + "                                    3600            \n"
+			    + "1 mile = 1.6 kilometers. \nAverage Speed in miles per hour = "
+			    + (14.0 / 1.6)/ (((45.0 * 60.0) + 30.0) / 3600) + " mph.");
+	  
+
+	}
+
+}
