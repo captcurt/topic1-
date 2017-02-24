c1d929df81822197fd6c2bba3c3e397fc2b10aee
captcurt <JCurtner1@my.gcu.edu>
2017-02-23 23:33:45 -0600

    Add files via upload
    Algebra equation on question 1.13

---
+++ Algebra.java
@@ -0,0 +1,13 @@
+
+public class Algebra {
+
+	public static void main(String[] args) { 
+		System.out.println("Soved the equastion of 3.4x+50.2y=44.5 2.1x+.55y=5.9");
+	    	        System.out.println("X is: ");
+	        System.out.println(((44.5 * 0.55) - (50.2 * 5.9))/((3.4 * 0.55) - (50.2 * 2.1)));
+	        System.out.println("Y is: ");
+	        System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) / ((3.4 * 0.55) - (50.2 * 2.1)));
+	
+	}
+       
+}
